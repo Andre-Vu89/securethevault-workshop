0a71901e2662ed52b3c041f1583d0e69b8d7078b
Andre Vu <andre.vu.ms@gmail.com>
2024-07-22 12:24:31 -0700

    adding git files for vault project to github

---
+++ script.js
@@ -0,0 +1,11 @@
+//Create prompt that states
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// 3 unique codes below
+let code1 = (9+1);
+let code2 = (45-5);
+let code3 = (40-1);
+
+//Pop up message
+console.log(`${message} ${code1} ${code2} ${code3}`);
+
